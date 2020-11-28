4c7766b88b9ca70ee3efeeab04d4b92af6d0ae66
bot196 <61917941+bot196@users.noreply.github.com>
2020-11-28 15:27:00 +0100

    Shop Activity

--- main/java/com/kc/ShopActivity.java
+++ main/java/com/kc/ShopActivity.java
@@ -3,8 +3,11 @@ package com.kc;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.kc.util.Stats;
 
 public class ShopActivity extends AppCompatActivity {
     @Override
@@ -14,6 +17,9 @@ public class ShopActivity extends AppCompatActivity {
 
         Button back = findViewById(R.id.back_to_main);
         Button more_cookies = findViewById(R.id.buy1);
+
+        TextView more_cookies_price = findViewById(R.id.price1);
+
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,7 +31,10 @@ public class ShopActivity extends AppCompatActivity {
         more_cookies.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                int price = (int) (Stats.MORE_COOKIES_PRICE * 2);
+                Stats.MORE_COOKIES_PRICE++;
+                Stats.COOKIES = Stats.COOKIES - price;
+                more_cookies_price.setText(String.valueOf(Stats.MORE_COOKIES_PRICE));
             }
         });
     }
--- main/java/com/kc/util/Stats.java
+++ main/java/com/kc/util/Stats.java
@@ -3,5 +3,7 @@ package com.kc.util;
 public class Stats {
 
     public static int COOKIES = 0;
-    public static int COOKIES_PLUS = 1;
+    public static int COOKIES_PLUS = 100;
+
+    public static int MORE_COOKIES_PRICE = 100;
 }
